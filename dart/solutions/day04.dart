9874a27b707c363cb753e2ccb2c388895232a3b8
Srishan Shakya <csunshakya@gmail.com>
2024-12-03 11:13:40 +0545

    dart: prepare day 4

---
+++ dart/solutions/day04.dart
@@ -0,0 +1,22 @@
+import '../utils/index.dart';
+
+class Day04 extends GenericDay {
+  Day04() : super(4);
+
+  @override
+  parseInput() {
+
+  }
+
+  @override
+  int solvePart1() {
+
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+
+    return 0;
+  }
+}
